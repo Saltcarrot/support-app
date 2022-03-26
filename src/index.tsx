490ff9a5cdb3aa88c6a8c61f2a823cc778e94642
Aleksandra <jofleytowaffles@gmail.com>
2022-03-26 14:40:43 +0500

    Added firebase config

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import firebase from 'firebase/compat/app'
 
 import Routes from './Routes'
 
 import './assets/sass/global.sass'
+
+firebase.initializeApp({
+	apiKey: process.env.REACT_APP_API_KEY,
+	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+	projectId: process.env.REACT_APP_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+	appId: process.env.REACT_APP_APP_ID,
+	databaseURL: process.env.REACT_APP_DB_URL,
+})
 
 ReactDOM.render(
 	<React.StrictMode>
