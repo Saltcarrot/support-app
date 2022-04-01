c9c99cd75f5ec5f30779ca22d4a3d1ce69719d2f
Aleksandra <jofleytowaffles@gmail.com>
2022-04-01 18:46:10 +0500

    Customized confirming password page

--- src/components/pages/Auth/Password/PasswordConfirm/PasswordConfirm.tsx
+++ src/components/pages/Auth/Password/PasswordConfirm/PasswordConfirm.tsx
@@ -1,7 +1,75 @@
 import { FC } from 'react'
+import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { IInput } from '../../../../../utils/types/input'
+import { updatePasswordSchema } from '../../../../../utils/helpers/validationSchemas'
+
+import AuthLayout from '../../../../common/Layout/AuthLayout/AuthLayout'
+import Container from '../../../../common/Container/Container'
+import { CustomForm as Form } from '../../../../common/UI/Form'
+
+import styles from '../../styles.module.sass'
 
 const PasswordConfirm: FC = () => {
-	return <div>PasswordConfirm</div>
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    mode: 'onSubmit',
+    resolver: yupResolver(updatePasswordSchema),
+  })
+
+  const onSubmit = (data: any) => {
+    console.log(data)
+  }
+
+  const confPassInputs: IInput[] = [
+    {
+      label: 'Введите пароль',
+      type: 'password',
+      name: 'password',
+      placement: 'auth',
+      placeholder: 'Пароль',
+    },
+    {
+      label: 'Повторите пароль',
+      type: 'password',
+      name: 'confirmPassword',
+      placement: 'auth',
+      placeholder: 'Повторите пароль',
+    },
+  ]
+
+  return (
+    <AuthLayout>
+      <Container content='auth'>
+        <Form.Container
+          title='Обновление пароля'
+          tip='Задайте новый пароль для своей учетной записи'
+          register={register}
+          errors={errors}
+          handleSubmit={handleSubmit}
+          onSubmit={onSubmit}
+          inputs={confPassInputs}
+          bottom={
+            <>
+              <div className={styles.wrapper_btns}>
+                <Form.Button.Google onClick={() => {}} />
+                <Form.Link.Redirect
+                  path='/authorization'
+                  title='Авторизоваться'
+                />
+              </div>
+              <div className={styles.wrapper_submit}>
+                <Form.Button.Submit text='Задать новый пароль' />
+              </div>
+            </>
+          }
+        />
+      </Container>
+    </AuthLayout>
+  )
 }
 
 export default PasswordConfirm
